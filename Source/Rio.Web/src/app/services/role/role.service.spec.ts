923ce0a68bdfd039ca8d2c732bbb29f3c7e7a0ee
raynardlee <raynardlee@yahoo.com>
2019-06-27 14:02:47 -0700

    minor refactorings

--- Source/Rio.Web/src/app/services/role/role.service.spec.ts
+++ Source/Rio.Web/src/app/services/role/role.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { SystemRoleService } from './role.service';
+import { RoleService } from './role.service';
 
 describe('SystemRoleService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: SystemRoleService = TestBed.get(SystemRoleService);
+    const service: RoleService = TestBed.get(RoleService);
     expect(service).toBeTruthy();
   });
 });
