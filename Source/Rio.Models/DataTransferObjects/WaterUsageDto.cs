cc4bc3b2bd1790cbf15b54fc9824958576d14d34
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-08-28 12:36:55 -0700

    [rio/#123] Add water use chart to Landowner dashboard Add DTOs for chart data to API

---
+++ Source/Rio.Models/DataTransferObjects/WaterUsageDto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Rio.Models.DataTransferObjects
+{
+    class WaterUsageDto
+    {
+        public int Year { get; set; }
+        public List<MonthlyWaterUsageDto> WaterUsage { get; set; }
+    }
+
+    public class MonthlyWaterUsageDto
+    {
+        public string Month { get; set; }
+        public List<ParcelWaterUsageDto> WaterUsageByParcel { get; set; }
+    }
+
+    public class ParcelWaterUsageDto
+    {
+        public string ParcelNumber { get; set; }
+        public double WaterUsageInAcreFeet { get; set; }
+    }
+}
