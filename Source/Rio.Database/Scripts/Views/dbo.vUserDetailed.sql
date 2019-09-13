eaa0935fbeeb7789563ede868ec9173c738699da
raynardlee <raynardlee@yahoo.com>
2019-09-13 10:51:06 -0700

    [rio/#149] Allow admins to edit text content forgot to add sql view for user grid Add a new label below Allocation, Purchased, and Sold called “Annual Supply (Allocation +/- Trades)”, which is calculated as: (Allocation + Purchased – Sold) Make the +/- on the landowner dashboard gray to distinguish from the label. When expanded, make the date a link to the trade detail page

---
+++ Source/Rio.Database/Scripts/Views/dbo.vUserDetailed.sql
@@ -0,0 +1,34 @@
+﻿if exists (select * from dbo.sysobjects where id = object_id('dbo.vUserDetailed'))
+	drop view dbo.vUserDetailed
+go
+
+create view dbo.vUserDetailed
+as
+
+select u.UserID, u.UserGuid, u.FirstName, u.LastName, u.Email, u.LoginName, u.Phone, u.Company, r.RoleID, r.RoleDisplayName,
+		cast(sign(isnull(au.UserID, 0)) as bit) as HasActiveTrades, 
+		sum(case when u.UserID = wt.ReceivingUserID then wt.AcreFeetTransferred else 0 end) as AcreFeetOfWaterPurchased,
+		sum(case when u.UserID = wt.TransferringUserID then wt.AcreFeetTransferred else 0 end) as AcreFeetOfWaterSold
+from dbo.[User] u
+join dbo.[Role] r on u.RoleID = r.RoleID
+left join (
+	select p.CreateUserID as UserID
+	from dbo.Posting p
+	where p.PostingStatusID = 1
+	union
+	select t.CreateUserID as UserID
+	from dbo.Trade t
+	where t.TradeStatusID = 1
+	union
+	select p.CreateUserID as UserID
+	from dbo.Offer p
+	where p.OfferStatusID = 1
+) au on u.UserID = au.UserID
+left join dbo.WaterTransfer wt on u.UserID = wt.ReceivingUserID or u.UserID = wt.TransferringUserID 
+group by u.UserID, u.UserGuid, u.FirstName, u.LastName, u.Email, u.LoginName, u.Phone, u.Company, r.RoleID, r.RoleDisplayName, au.UserID
+
+GO
+/*
+select *
+from dbo.vUserDetailed
+*/
