ba8546a649b653a36ae6c3d46ce4eaff9e1151c8
raynardlee <raynardlee@yahoo.com>
2019-09-20 14:08:41 -0700

    [rio/#152] Add Postings grid to Manager Dashboard bad join on postingID

--- Source/Rio.Database/Scripts/Views/dbo.vPostingDetailed.sql
+++ Source/Rio.Database/Scripts/Views/dbo.vPostingDetailed.sql
@@ -5,20 +5,20 @@ go
 create view dbo.vPostingDetailed
 as
 
-select p.PostingID, p.PostingDate, pt.PostingTypeID, pt.PostingTypeDisplayName
-, ps.PostingStatusID, ps.PostingStatusDisplayName
-, u.UserID as PostedByUserID, u.FirstName as PostedByFirstName, u.LastName as PostedByLastName, u.Email as PostedByEmail
-, p.Price, p.Quantity, p.AvailableQuantity
-, count(t.TradeID) as NumberOfOffers
-from dbo.Posting p
-join dbo.PostingType pt on p.PostingTypeID = pt.PostingTypeID
-join dbo.PostingStatus ps on p.PostingStatusID = ps.PostingStatusID
-left join dbo.Trade t on p.PostingID = t.TradeID
-left join dbo.[User] u on p.CreateUserID = u.UserID
-group by p.PostingID, p.PostingDate, pt.PostingTypeID, pt.PostingTypeDisplayName
-, ps.PostingStatusID, ps.PostingStatusDisplayName
-, u.UserID, u.FirstName, u.LastName, u.Email
-, p.Price, p.Quantity, p.AvailableQuantity
+	select p.PostingID, p.PostingDate, pt.PostingTypeID, pt.PostingTypeDisplayName
+	, ps.PostingStatusID, ps.PostingStatusDisplayName
+	, u.UserID as PostedByUserID, u.FirstName as PostedByFirstName, u.LastName as PostedByLastName, u.Email as PostedByEmail
+	, p.Price, p.Quantity, p.AvailableQuantity
+	, count(t.TradeID) as NumberOfOffers
+	from dbo.Posting p
+	join dbo.PostingType pt on p.PostingTypeID = pt.PostingTypeID
+	join dbo.PostingStatus ps on p.PostingStatusID = ps.PostingStatusID
+	left join dbo.Trade t on p.PostingID = t.PostingID
+	left join dbo.[User] u on p.CreateUserID = u.UserID
+	group by p.PostingID, p.PostingDate, pt.PostingTypeID, pt.PostingTypeDisplayName
+	, ps.PostingStatusID, ps.PostingStatusDisplayName
+	, u.UserID, u.FirstName, u.LastName, u.Email
+	, p.Price, p.Quantity, p.AvailableQuantity
 
 GO
 /*
