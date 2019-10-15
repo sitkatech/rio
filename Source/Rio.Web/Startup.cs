63e6ff4d005066ebe8f725754572b1868a4bf0a1
raynardlee <raynardlee@yahoo.com>
2019-10-15 16:18:20 -0700

    removing need for mvc

--- Source/Rio.Web/Startup.cs
+++ Source/Rio.Web/Startup.cs
@@ -30,7 +30,6 @@ namespace Rio.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,7 +53,6 @@ namespace Rio.Web
                 }
             });
 
-            app.UseMvcWithDefaultRoute();
             app.UseDefaultFiles();
             app.UseStaticFiles();
         }
