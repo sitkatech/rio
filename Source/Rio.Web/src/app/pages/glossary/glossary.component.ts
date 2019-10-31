b8361456652a6681410f39caeacc46aeb4c06a5b
raynardlee <raynardlee@yahoo.com>
2019-10-31 16:30:37 -0700

    forgot to add new glossary page

---
+++ Source/Rio.Web/src/app/pages/glossary/glossary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rio-glossary',
+  templateUrl: './glossary.component.html',
+  styleUrls: ['./glossary.component.scss']
+})
+export class GlossaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
