cb63359a887c35fa04a4faf2cd6842af18f3aea7
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2019-12-24 12:52:06 -0800

    Update vUserDetail with an intermediate join on the AccountUser table

--- Source/Rio.Database/Scripts/Views/dbo.vUserDetailed.sql
+++ Source/Rio.Database/Scripts/Views/dbo.vUserDetailed.sql
@@ -6,27 +6,29 @@ create view dbo.vUserDetailed
 as
 
 select u.UserID, u.UserGuid, u.FirstName, u.LastName, u.Email, u.LoginName, u.Phone, u.Company, r.RoleID, r.RoleDisplayName,
-		cast(sign(isnull(au.UserID, 0)) as bit) as HasActiveTrades, 
+		cast(sign(isnull(zz.AccountID, 0)) as bit) as HasActiveTrades, 
 		sum(case when wtr.WaterTransferTypeID = 1 then wt.AcreFeetTransferred else 0 end) as AcreFeetOfWaterPurchased,
 		sum(case when wtr.WaterTransferTypeID = 2 then wt.AcreFeetTransferred else 0 end) as AcreFeetOfWaterSold
 from dbo.[User] u
 join dbo.[Role] r on u.RoleID = r.RoleID
+left join dbo.AccountUser au
+	on u.UserID = au.UserID
 left join (
-	select p.CreateUserID as UserID
+	select p.CreateAccountID as AccountID
 	from dbo.Posting p
 	where p.PostingStatusID = 1
 	union
-	select t.CreateUserID as UserID
+	select t.CreateAccountID as AccountID
 	from dbo.Trade t
 	where t.TradeStatusID = 1
 	union
-	select p.CreateUserID as UserID
+	select p.CreateAccountID as AccountID
 	from dbo.Offer p
 	where p.OfferStatusID = 1
-) au on u.UserID = au.UserID
-left join dbo.WaterTransferRegistration wtr on u.UserID = wtr.UserID
+) zz on au.AccountID = zz.AccountID
+left join dbo.WaterTransferRegistration wtr on au.AccountID = wtr.AccountID
 left join dbo.WaterTransfer wt on wtr.WaterTransferID = wt.WaterTransferID
-group by u.UserID, u.UserGuid, u.FirstName, u.LastName, u.Email, u.LoginName, u.Phone, u.Company, r.RoleID, r.RoleDisplayName, au.UserID
+group by u.UserID, u.UserGuid, u.FirstName, u.LastName, u.Email, u.LoginName, u.Phone, u.Company, r.RoleID, r.RoleDisplayName, zz.AccountID
 
 GO
 /*
