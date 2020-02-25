f7fa579c6ba447df8c46ee8489c785e881e3419f
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2020-02-25 10:37:02 -0800

    Fixed

--- Source/Rio.Database/Scripts/Views/dbo.vUserDetailed.sql
+++ Source/Rio.Database/Scripts/Views/dbo.vUserDetailed.sql
@@ -6,7 +6,7 @@ create view dbo.vUserDetailed
 as
 
 select u.UserID, u.UserGuid, u.FirstName, u.LastName, u.Email, u.LoginName, u.Phone, u.Company, u.ReceiveSupportEmails, r.RoleID, r.RoleDisplayName,
-		cast(sign(isnull(zz.AccountID, 0)) as bit) as HasActiveTrades, 
+		cast(sign(isnull(sum(zz.AccountID), 0)) as bit) as HasActiveTrades, 
 		sum(case when wtr.WaterTransferTypeID = 1 then wt.AcreFeetTransferred else 0 end) as AcreFeetOfWaterPurchased,
 		sum(case when wtr.WaterTransferTypeID = 2 then wt.AcreFeetTransferred else 0 end) as AcreFeetOfWaterSold
 from dbo.[User] u
@@ -14,6 +14,7 @@ join dbo.[Role] r on u.RoleID = r.RoleID
 left join dbo.AccountUser au
 	on u.UserID = au.UserID
 left join (
+-- This is probably a wee bit more complicated than it needs to be?
 	select p.CreateAccountID as AccountID
 	from dbo.Posting p
 	where p.PostingStatusID = 1
@@ -28,7 +29,7 @@ left join (
 ) zz on au.AccountID = zz.AccountID
 left join dbo.WaterTransferRegistration wtr on au.AccountID = wtr.AccountID
 left join dbo.WaterTransfer wt on wtr.WaterTransferID = wt.WaterTransferID
-group by u.UserID, u.UserGuid, u.FirstName, u.LastName, u.Email, u.LoginName, u.Phone, u.Company, u.ReceiveSupportEmails, r.RoleID, r.RoleDisplayName, zz.AccountID
+group by u.UserID, u.UserGuid, u.FirstName, u.LastName, u.Email, u.LoginName, u.Phone, u.Company, u.ReceiveSupportEmails, r.RoleID, r.RoleDisplayName
 
 GO
 /*
