c64cea872578e45d4d4191936e7c5a03ab45bed5
Mack Peters <mack.peters@sitkatech.com>
2020-03-31 11:17:49 -0700

    Adding tables and views for disadvantaged community layer

---
+++ Database/Objects/Views/dbo.vGeoServerDisadvantagedCommunity.sql
@@ -0,0 +1,20 @@
+﻿if exists (select * from dbo.sysobjects where id = object_id('dbo.vGeoServerDisadvantagedCommunity'))
+	drop view dbo.vGeoServerDisadvantagedCommunity
+go
+
+create view dbo.vGeoServerDisadvantagedCommunity
+as
+
+select      dc.DisadvantagedCommunityID as PrimaryKey,
+			dc.DisadvantagedCommunityName,
+			dc.LSADCode,
+			dcs.DisadvantagedCommunityStatusName,
+			dcs.GeoServerLayerColor   
+                
+FROM        dbo.DisadvantagedCommunity dc
+JOIN		dbo.DisadvantagedCommunityStatus dcs on dc.DisadvantagedCommunityStatusID = dcs.DisadvantagedCommunityStatusID
+
+GO
+/*
+select * from dbo.vGeoServerDisadvantagedCommunity
+*/
