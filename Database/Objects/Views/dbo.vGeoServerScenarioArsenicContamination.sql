7d60c67a1b783c1ad89d986f7cec5db2e3c905b3
Mack Peters <mack.peters@sitkatech.com>
2020-04-13 16:33:49 -0700

    GeoServer views for ArsenicContamination and RechargeBasin

---
+++ Database/Objects/Views/dbo.vGeoServerScenarioArsenicContamination.sql
@@ -0,0 +1,27 @@
+﻿if exists (select * from dbo.sysobjects where id = object_id('dbo.vGeoServerScenarioArsenicContamination'))
+	drop view dbo.vGeoServerScenarioArsenicContamination
+go
+
+create view dbo.vGeoServerScenarioArsenicContamination
+as
+
+select      ac.ScenarioArsenicContaminationID as PrimaryKey,
+			ac.ScenarioArsenicContaminationGeometry,
+			ac.ScenarioArsenicContaminationWellID,
+			ac.ScenarioArsenicContaminationContaminationConcentration,
+			ac.ScenarioArsenicContaminationDateAssessed,
+			ac.ScenarioArsenicContaminationUnits,
+			ac.ScenarioArsenicContaminationLatitude,
+			ac.ScenarioArsenicContaminationLongitude,
+			acwt.ScenarioArsenicContaminationWellTypeName,
+			acs.ScenarioArsenicContaminationSourceName
+
+                
+FROM        dbo.ScenarioArsenicContamination ac
+JOIN		dbo.ScenarioArsenicContaminationWellType acwt on ac.ScenarioArsenicContaminationWellTypeID = ac.ScenarioArsenicContaminationWellTypeID
+JOIN		dbo.ScenarioArsenicContaminationSource acs on ac.ScenarioArsenicContaminationSourceID = acs.ScenarioArsenicContaminationSourceID
+
+GO
+/*
+select * from dbo.vGeoServerWaterTradingScenarioWell
+*/
---
+++ Database/Objects/Views/dbo.vGeoServerScenarioRechargeBasin.sql
@@ -0,0 +1,18 @@
+﻿if exists (select * from dbo.sysobjects where id = object_id('dbo.vGeoServerScenarioRechargeBasin'))
+	drop view dbo.vGeoServerScenarioRechargeBasin
+go
+
+create view dbo.vGeoServerScenarioRechargeBasin
+as
+
+select      rb.ScenarioRechargeBasinID as PrimaryKey,
+			rb.ScenarioRechargeBasinGeometry,
+			rb.ScenarioRechargeBasinName,
+			rb.ScenarioRechargeBasinAcres,
+			rb.ScenarioRechargeBasinCapacity,
+			rb.ScenarioRechargeBasinBasinName
+
+                
+FROM        dbo.ScenarioRechargeBasin rb
+
+GO
