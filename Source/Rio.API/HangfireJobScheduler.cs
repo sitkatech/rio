e4c6cdffb53c416faace568acbeaa0eb19821192
SITKA\nick.padinha <nick.padinha@sitkatech.com>
2020-10-29 15:29:56 -0700

    Fix Hangfire/DST stuff

--- Source/Rio.API/HangfireJobScheduler.cs
+++ Source/Rio.API/HangfireJobScheduler.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using NetTopologySuite.Utilities;
 using static Hangfire.JobCancellationToken;
 
 namespace Rio.API
@@ -15,7 +16,7 @@ namespace Rio.API
             var recurringJobIds = new List<string>();
 
             // todo: what's the right cron
-            AddRecurringJob<CimisPrecipJob>(CimisPrecipJob.JobName, x => x.RunJob(Null), Cron.Daily(1, 30), recurringJobIds);
+            AddRecurringJob<CimisPrecipJob>(CimisPrecipJob.JobName, x => x.RunJob(Null), MakeDailyUtcCronJobStringFromLocalTime(1, 30), recurringJobIds);
 
             // Remove any jobs we haven't explicitly scheduled
             RemoveExtraneousJobs(recurringJobIds);
@@ -37,6 +38,65 @@ namespace Rio.API
             {
                 RecurringJob.RemoveIfExists(job.Id);
             }
+        }
+        /// <summary>
+        /// Hangfire defaults to a UTC time, so here convert from local time to UTC, then use the equivalent
+        /// UTC time to create a cron string.
+        /// 
+        /// Since SetUpBackgroundHangfireJobs should be re-run when the webserver restarts, this should get
+        /// updated enough to handle the problems associated with DST/UTC/TimeZone conversions. At the least,
+        /// problems won't hang around for too long since AddOrUpdate will adjust the time to be the correct one
+        /// after a DST change. -- SLG 03/16/2015
+        /// </summary>
+        private static string MakeDailyUtcCronJobStringFromLocalTime(int hour, int minute)
+        {
+            var utcCronTime = MakeUtcCronTime(hour, minute);
+            return Cron.Daily(utcCronTime.Hour, utcCronTime.Minute);
+        }
+
+        private static string MakeWeeklyUtcCronJobStringFromLocalTime(int hour, int minute, DayOfWeek dayOfWeek)
+        {
+            var utcCronTime = MakeUtcCronTime(hour, minute);
+            return Cron.Weekly(dayOfWeek, utcCronTime.Hour, utcCronTime.Minute);
+        }
+
+        private static string MakeYearlyUtcCronJobStringFromLocalTime(int month, int day, int hour, int minute)
+        {
+            var utcCronTime = MakeUtcCronTime(month, day, hour, minute);
+            return Cron.Yearly(utcCronTime.Month, utcCronTime.Day, utcCronTime.Hour, utcCronTime.Minute);
+        }
+
+        private static DateTime MakeUtcCronTime(int hour, int minute)
+        {
+            var now = DateTime.Now;
+            return MakeUtcCronTime(now.Year, now.Month, now.Day, hour, minute);
+        }
+
+        private static DateTime MakeUtcCronTime(int month, int day, int hour, int minute)
+        {
+            var now = DateTime.Now;
+            return MakeUtcCronTime(now.Year, month, day, hour, minute);
+        }
+
+        public static DateTime MakeUtcCronTime(int year, int month, int day, int hour, int minute)
+        {
+            TimeZoneInfo tz = TimeZoneInfo.Local; // getting the current system timezone
+            DateTime localCronTime = new DateTime(year, month, day, hour, minute, 0, DateTimeKind.Local);
+
+            // Catch cases where time is invalid or ambiguous due to daylight savings
+            // 3/8/20 2am becomes 3am (so 2am – 3am in invalid)
+            // 11/1/20 2am becomes 1am(so 1am – 2am is ambiguous)
+            if (tz.IsAmbiguousTime(localCronTime) || tz.IsInvalidTime(localCronTime))
+            {
+                localCronTime = localCronTime.Add(TimeSpan.Parse("1:01:00"));
+
+                // Make sure we've fixed the issue
+                Assert.IsTrue(!tz.IsAmbiguousTime(localCronTime)); 
+                Assert.IsTrue(!tz.IsInvalidTime(localCronTime));
+            }
+
+            var utcCronTime = TimeZoneInfo.ConvertTimeToUtc(localCronTime);
+            return utcCronTime;
         }
     }
 }
