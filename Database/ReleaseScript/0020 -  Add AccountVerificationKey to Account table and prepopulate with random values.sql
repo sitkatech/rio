c9400a13a4f9618637788999be07b7bf5e7c0ba4
Mack Peters <mack.peters@sitkatech.com>
2020-11-04 10:07:24 -0800

    Simplify release script, caught bad logic in detail page

--- Database/ReleaseScript/0020 -  Add AccountVerificationKey to Account table and prepopulate with random values.sql
+++ Database/ReleaseScript/0020 -  Add AccountVerificationKey to Account table and prepopulate with random values.sql
@@ -1,54 +1,14 @@
-create table dbo.AccountTemp(
-	RowNum int null,
-	AccountID int null,
-	AccountVerificationKey varchar(6) null
-)
-
-insert into dbo.AccountTemp (RowNum, AccountID)
-select ROW_NUMBER() OVER(ORDER BY AccountID ASC) AS RowNum, AccountID
-from dbo.Account
-go
-
-DECLARE @CurrentRowNum INT = 1;
-DECLARE @RowCount BIGINT;
-DECLARE @CountContainingKey INT = 1;
-DECLARE @NewKey varchar(6);
-
-DECLARE @AlphaUpperBound INT = 27;
-DECLARE @AlphaLowerBound INT = 1;
-DECLARE @IntUpperBound INT = 10;
-DECLARE @IntLowerBound INT =  0;
-
-SELECT @RowCount = COUNT(*) FROM dbo.AccountTemp;
- 
-WHILE @CurrentRowNum <= @RowCount
-BEGIN
-   WHILE @CountContainingKey > 0
-   BEGIN
-		SET @NewKey = (char(64 + ROUND(((@AlphaUpperBound-@AlphaLowerBound-1) * RAND() + @AlphaLowerBound), 0)) 
-					+ char(64 + ROUND(((@AlphaUpperBound-@AlphaLowerBound-1) * RAND() + @AlphaLowerBound), 0))
-					+ char(64 + ROUND(((@AlphaUpperBound-@AlphaLowerBound-1) * RAND() + @AlphaLowerBound), 0))
-					+ CAST(ROUND(((@IntUpperBound-@IntLowerBound-1) * RAND()), + @IntLowerBound) as varchar)
-					+ CAST(ROUND(((@IntUpperBound-@IntLowerBound-1) * RAND()), + @IntLowerBound) as varchar)
-					+ CAST(ROUND(((@IntUpperBound-@IntLowerBound-1) * RAND()), + @IntLowerBound) as varchar))
-		SELECT @CountContainingKey = COUNT(*) FROM dbo.AccountTemp WHERE AccountVerificationKey = @NewKey
-   END
-   UPDATE dbo.AccountTemp 
-   SET AccountVerificationKey = @NewKey
-   WHERE RowNum = @CurrentRowNum;
-    
-   SET @CurrentRowNum = @CurrentRowNum + 1 
-   SET @CountContainingKey = 1
-END
-
 alter table dbo.Account
 add AccountVerificationKey varchar(6) null
 go
 
 update dbo.Account
-set AccountVerificationKey = [at].AccountVerificationKey
-from dbo.AccountTemp [at]
-join dbo.Account a on [at].AccountID = a.AccountID
+set AccountVerificationKey = concat(
+	char(65 + cast(substring(cast(AccountNumber as varchar), 5, 1) as int)),
+	char(65 + cast(substring(cast(AccountNumber as varchar), 4, 1) as int)),
+	char(65 + cast(substring(cast(AccountNumber as varchar), 3, 1) as int)),
+	RIGHT('000'+CAST((AccountID * AccountNumber) % 1000 AS VARCHAR(3)),3)
+	)
 go
 
 alter table dbo.Account
@@ -57,7 +17,4 @@ go
 
 alter table dbo.Account
 add constraint AK_Account_AccountVerificationKey unique(AccountVerificationKey)
-go
-
-drop table dbo.AccountTemp
 go
