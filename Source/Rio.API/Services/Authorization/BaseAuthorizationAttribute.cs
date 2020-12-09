71616430caa218807d873fb9cb16d71c54664252
Mack Peters <mack.peters@sitkatech.com>
2020-12-09 09:11:54 -0800

    Cleanup

--- Source/Rio.API/Services/Authorization/BaseAuthorizationAttribute.cs
+++ Source/Rio.API/Services/Authorization/BaseAuthorizationAttribute.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Rio.EFModels.Entities;
