77fa64779946706292ea23e28271a3e94b216613
Mack Peters <mack.peters@sitkatech.com>
2020-12-29 08:46:42 -0800

    Changes to facilitate updating for WaterYear (choosing which year to make changes applicable) and Account (Date account created, date account inactivated)

--- Database/ReleaseScript/0033 - Add CreateDate and InactivateDate to Account, make AccountVerificationKey nullable.sql
+++ Database/ReleaseScript/0033 - Add CreateDate and InactivateDate to Account, make AccountVerificationKey nullable.sql
@@ -22,8 +22,4 @@ drop constraint [AK_Account_AccountVerificationKey]
 
 create unique nonclustered index [AK_Account_AccountVerificationKey] 
 on dbo.Account(AccountVerificationKey)
-<<<<<<< HEAD
 where AccountVerificationKey is not null
-=======
-where AccountVerificationKey is not null
->>>>>>> Found out initial parcel layer was projected in 32611, and under that assumption can now fully complete operation. Add helpers to cast 32611 to 4326, add script that does the parcel layer update officially, add constraint to ensure that any account that is Inactivated has an Inactivate date and any account that isn't inactive doesn't have an inactivate date, make account verification key nullable but add constraint to enforce uniqueness, remove constraint from accountparcel table that requires either an accountid or ownername (may go back on this later)
