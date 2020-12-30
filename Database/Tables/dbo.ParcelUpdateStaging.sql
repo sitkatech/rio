90176a4e93d7519c73c1a8d8e0fae278eb106822
Mack Peters <mack.peters@sitkatech.com>
2020-12-30 10:27:52 -0800

    Found out initial parcel layer was projected in 32611, and under that assumption can now fully complete operation. Add helpers to cast 32611 to 4326, add script that does the parcel layer update officially, add constraint to ensure that any account that is Inactivated has an Inactivate date and any account that isn't inactive doesn't have an inactivate date, make account verification key nullable but add constraint to enforce uniqueness, remove constraint from accountparcel table that requires either an accountid or ownername (may go back on this later)

--- Database/Tables/dbo.ParcelUpdateStaging.sql
+++ Database/Tables/dbo.ParcelUpdateStaging.sql
@@ -5,9 +5,13 @@ GO
 CREATE TABLE [dbo].[ParcelUpdateStaging](
 	[ParcelUpdateStagingID] [int] IDENTITY(1,1) NOT NULL,
 	[ParcelNumber] [varchar](20) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
-	[ParcelGeometry] [geometry] NULL,
+	[ParcelGeometry] [geometry] NOT NULL,
 	[OwnerName] [varchar](100) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+<<<<<<< HEAD
 	[ParcelGeometry4326] [geometry] NULL,
+=======
+	[ParcelGeometry4326] [geometry] NOT NULL,
+>>>>>>> Found out initial parcel layer was projected in 32611, and under that assumption can now fully complete operation. Add helpers to cast 32611 to 4326, add script that does the parcel layer update officially, add constraint to ensure that any account that is Inactivated has an Inactivate date and any account that isn't inactive doesn't have an inactivate date, make account verification key nullable but add constraint to enforce uniqueness, remove constraint from accountparcel table that requires either an accountid or ownername (may go back on this later)
 	[ParcelGeometryText] [varchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
 	[ParcelGeometry4326Text] [varchar](max) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
  CONSTRAINT [PK_ParcelUpdateStaging_ParcelUpdateStagingID] PRIMARY KEY CLUSTERED 
