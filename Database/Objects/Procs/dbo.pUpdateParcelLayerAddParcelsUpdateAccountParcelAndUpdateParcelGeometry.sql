667f4d416b1ac28f196e1213f342b438fe6af75f
Mack Peters <mack.peters@sitkatech.com>
2020-12-30 10:27:52 -0800

    Found out initial parcel layer was projected in 32611, and under that assumption can now fully complete operation. Add helpers to cast 32611 to 4326, add script that does the parcel layer update officially, add constraint to ensure that any account that is Inactivated has an Inactivate date and any account that isn't inactive doesn't have an inactivate date, make account verification key nullable but add constraint to enforce uniqueness, remove constraint from accountparcel table that requires either an accountid or ownername (may go back on this later)

---
+++ Database/Objects/Procs/dbo.pUpdateParcelLayerAddParcelsUpdateAccountParcelAndUpdateParcelGeometry.sql
@@ -0,0 +1,31 @@
+IF EXISTS(SELECT 1 FROM sys.objects WHERE object_id = OBJECT_ID(N'dbo.pUpdateParcelLayerAddParcelsUpdateAccountParcelAndUpdateParcelGeometry'))
+    drop procedure dbo.pUpdateParcelLayerAddParcelsUpdateAccountParcelAndUpdateParcelGeometry
+go
+
+create procedure dbo.pUpdateParcelLayerAddParcelsUpdateAccountParcelAndUpdateParcelGeometry
+as
+
+begin
+
+	insert into dbo.Parcel (ParcelNumber, ParcelGeometry, OwnerName, OwnerAddress, OwnerCity, OwnerZipCode, ParcelAreaInSquareFeet, ParcelAreaInAcres)
+	select pus.ParcelNumber, pus.ParcelGeometry4326, pus.OwnerName, 'a', 'a', 'a', round(pus.ParcelGeometry.STArea() * 10.764, 0), round(pus.ParcelGeometry.STArea() / 4047, 14) 
+	from dbo.vParcelLayerUpdateDifferencesInAccountAssociatedWithParcelAndParcelGeometry v
+	join dbo.ParcelUpdateStaging pus on v.ParcelNumber = pus.ParcelNumber
+	where OldOwnerName is null and OldGeometryText is null
+
+
+	insert into dbo.AccountParcel (AccountID, ParcelID, EffectiveYear, SaleDate)
+	select a.AccountID, p.ParcelID, 2020, GETDATE()
+	from dbo.vParcelLayerUpdateDifferencesInAccountAssociatedWithParcelAndParcelGeometry v
+	join dbo.Parcel p on v.ParcelNumber = p.ParcelNumber
+	left join dbo.Account a on v.NewOwnerName = a.AccountName
+	where (OldOwnerName is null and NewOwnerName is not null) or OldOwnerName <> NewOwnerName
+
+	update dbo.Parcel
+	set ParcelGeometry = pus.ParcelGeometry4326,
+	ParcelAreaInSquareFeet = round(pus.ParcelGeometry.STArea() * 10.764, 0),
+	ParcelAreaInAcres = round(pus.ParcelGeometry.STArea() / 4047, 14)
+	from dbo.ParcelUpdateStaging pus
+	join dbo.Parcel p on pus.ParcelNumber = p.ParcelNumber
+
+end
