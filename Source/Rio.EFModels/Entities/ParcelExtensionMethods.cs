c1b02228be02d48bb37d999863f74bf1953f2ea4
Mack Peters <mack.peters@sitkatech.com>
2020-12-31 14:12:19 -0800

    Restructure ParcelStatus to be associated with  AccountParcel since that will be the best way to track changes in  the status, add column in Parcel list page for parcel status

--- Source/Rio.EFModels/Entities/ParcelExtensionMethods.cs
+++ Source/Rio.EFModels/Entities/ParcelExtensionMethods.cs
@@ -12,7 +12,6 @@ namespace Rio.EFModels.Entities
             return new ParcelDto()
             {
                 ParcelID = parcel.ParcelID,
-                ParcelStatus = parcel.ParcelStatus.AsDto(),
                 ParcelNumber = parcel.ParcelNumber,
                 ParcelAreaInAcres = parcel.ParcelAreaInAcres,
                 LandOwner = parcel.AccountParcel.Where(x=> x.EffectiveYear <= currentYear).OrderByDescending(x => x.SaleDate).FirstOrDefault()?.Account?.AsSimpleDto()
