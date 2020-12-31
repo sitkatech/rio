6bb4a1f8a0a327201fbb961736efb9588c1ad4d9
Mack Peters <mack.peters@sitkatech.com>
2020-12-30 17:08:08 -0800

    Add Water Year selection to the mix

--- Source/Rio.API/Controllers/ParcelController.cs
+++ Source/Rio.API/Controllers/ParcelController.cs
@@ -486,12 +486,34 @@ namespace Rio.API.Controllers
         }
 
         [HttpPost("/parcels/enactGDBChanges")]
-        public ActionResult EnactGDBChanges()
+        public ActionResult EnactGDBChanges([FromBody] int waterYearID)
         {
             if (!_dbContext.ParcelUpdateStaging.Any())
             {
                 return BadRequest(
                     "An error occurred and the changes could not be completed. Please re-upload the GDB and try again.");
+            }
+
+            var waterYearDto = WaterYear.GetByWaterYearID(_dbContext, waterYearID);
+            
+            if (waterYearDto == null)
+            {
+                return BadRequest(
+                    "There was an error applying these changes to the selected Water Year. Please try again, and if the problem persists contact support.");
+            }
+
+            var currentWaterYearDto = WaterYear.GetByYear(_dbContext, DateTime.Now.Year);
+
+            if (currentWaterYearDto.Year - waterYearDto.Year > 1)
+            {
+                return BadRequest(
+                    "Changes may only be applied to the current year or the previous year. Please update Water Year selection and try again.");
+            }
+
+            if (waterYearDto.Year != currentWaterYearDto.Year && currentWaterYearDto.ParcelLayerUpdateDate != null)
+            {
+                return BadRequest(
+                    "Because changes have been applied to the current year previously, you may only select the current year to apply these changes to. Please update Water Year selection and try again.");
             }
 
             using var dbContextTransaction = _dbContext.Database.BeginTransaction();
@@ -525,7 +547,9 @@ namespace Rio.API.Controllers
                 }
 
                 _dbContext.Database.ExecuteSqlRaw(
-                    "EXECUTE dbo.pUpdateParcelLayerAddParcelsUpdateAccountParcelAndUpdateParcelGeometry");
+                    "EXECUTE dbo.pUpdateParcelLayerAddParcelsUpdateAccountParcelAndUpdateParcelGeometry {0}", waterYearDto.Year);
+
+                WaterYear.UpdateParcelLayerUpdateDateForID(_dbContext, waterYearID);
 
                 dbContextTransaction.Commit();
             }
