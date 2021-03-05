11dc1bdf2b9fc52075ece7d753450a9180295d9d
Mack Peters <mack.peters@sitkatech.com>
2021-03-05 07:27:01 -0800

    Cleanup

--- Source/Rio.API/Controllers/ParcelController.cs
+++ Source/Rio.API/Controllers/ParcelController.cs
@@ -72,12 +72,6 @@ namespace Rio.API.Controllers
             return Ok(parcelDtosWithAllocation);
         }
 
-        [HttpGet("test")]
-        public void Test()
-        {
-            
-        }
-
         [HttpGet("parcels/inactive")]
         [ManagerDashboardFeature]
         public ActionResult<IEnumerable<ParcelDto>> GetInactiveParcels()
