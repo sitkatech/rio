aa767c1a78206eb191da89ab35a2c21407ce5b0a
Andy Schultheiss <andy.schultheiss@sitkatech.com>
2021-10-04 12:00:46 -0700

    Amended stored procedure pParcelAllocationAndUsage to use ParcelLedger data

--- Database/Objects/Procs/dbo.pParcelAllocationAndUsage.sql
+++ Database/Objects/Procs/dbo.pParcelAllocationAndUsage.sql
@@ -12,27 +12,25 @@ begin
 
 	select p.ParcelID, p.ParcelNumber, p.ParcelAreaInAcres,
 			pal.Allocation, pal.ProjectWater, pal.Reconciliation, pal.NativeYield, pal.StoredWater,
-			pmev.UsageToDate, a.AccountID, a.AccountName, a.AccountNumber
+			pal.UsageToDate, a.AccountID, a.AccountName, a.AccountNumber
 	from dbo.Parcel p
 	join dbo.AccountParcelWaterYear apwy on p.ParcelID = apwy.ParcelID
 	join dbo.WaterYear wy on apwy.WaterYearID = wy.WaterYearID and wy.[Year] = @year
 	join dbo.[Account] a on apwy.AccountID = a.AccountID
 	left join 
 	(
-		select pa.ParcelID, 
-			isnull(sum(pa.AcreFeetAllocated), 0) as Allocation, 
-			sum(case when pa.ParcelAllocationTypeID = 1 then pa.AcreFeetAllocated else 0 end) as ProjectWater,
-			sum(case when pa.ParcelAllocationTypeID = 2 then pa.AcreFeetAllocated else 0 end) as Reconciliation,
-			sum(case when pa.ParcelAllocationTypeID = 3 then pa.AcreFeetAllocated else 0 end) as NativeYield,
-			sum(case when pa.ParcelAllocationTypeID = 4 then pa.AcreFeetAllocated else 0 end) as StoredWater
-		from dbo.ParcelAllocation pa where pa.WaterYear = @year
-		group by pa.ParcelID
+		select pl.ParcelID, 
+			isnull(sum(case when pl.TransactionTypeID in (11,12,13,14,15,16) then pl.TransactionAmount else 0 end), 0) as Allocation, 
+			sum(case when pl.TransactionTypeID = 11 then pl.TransactionAmount else 0 end) as ProjectWater,
+			sum(case when pl.TransactionTypeID = 12 then pl.TransactionAmount else 0 end) as Reconciliation,
+			sum(case when pl.TransactionTypeID = 13 then pl.TransactionAmount else 0 end) as NativeYield,
+			sum(case when pl.TransactionTypeID = 14 then pl.TransactionAmount else 0 end) as StoredWater,
+			sum(case when pl.TransactionTypeID in (17,18) then abs(pl.TransactionAmount) else 0 end) as UsageToDate
+
+		from dbo.ParcelLedger pl 
+		join dbo.TransactionType tt on pl.TransactionTypeID = tt.TransactionTypeID
+		where year(pl.TransactionDate) = @year
+		group by pl.ParcelID
 	) pal on p.ParcelID = pal.ParcelID
-	left join 
-	(
-		select pme.ParcelID, isnull(sum(coalesce(pme.OverriddenEvapotranspirationRate, pme.EvapotranspirationRate)), 0) as UsageToDate
-		from dbo.ParcelMonthlyEvapotranspiration pme where pme.WaterYear = @year
-		group by pme.ParcelID
-	) pmev on p.ParcelID = pmev.ParcelID
 
 end
