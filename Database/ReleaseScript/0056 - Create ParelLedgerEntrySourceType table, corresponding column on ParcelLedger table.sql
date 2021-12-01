e09a48d501441a6571f5c481b799c56378c159d6
raynardlee <raynardlee@yahoo.com>
2021-12-01 09:23:04 -0800

    adding DisplayName to ParcelLedgerEntrySource removing default from ParcelLedger.ParcelLedgerEntrySourceID

--- Database/ReleaseScript/0056 - Create ParelLedgerEntrySourceType table, corresponding column on ParcelLedger table.sql
+++ Database/ReleaseScript/0056 - Create ParelLedgerEntrySourceType table, corresponding column on ParcelLedger table.sql
@@ -1,25 +1,30 @@
 create table dbo.ParcelLedgerEntrySourceType
 (
 	ParcelLedgerEntrySourceTypeID int not null constraint PK_ParcelLedgerEntrySourceType_SourceTypeID primary key,
-	ParcelLedgerEntrySourceTypeName varchar (50) not null
+	ParcelLedgerEntrySourceTypeName varchar (50) not null constraint AK_ParcelLedgerEntrySourceType_ParcelLedgerEntrySourceTypeName unique,
+	ParcelLedgerEntrySourceTypeDisplayName varchar (50) not null constraint AK_ParcelLedgerEntrySourceType_ParcelLedgerEntrySourceTypeDisplayName unique
 )
 
 go 
 
-insert into dbo.ParcelLedgerEntrySourceType (ParcelLedgerEntrySourceTypeID, ParcelLedgerEntrySourceTypeName)
+insert into dbo.ParcelLedgerEntrySourceType (ParcelLedgerEntrySourceTypeID, ParcelLedgerEntrySourceTypeName, ParcelLedgerEntrySourceTypeDisplayName)
 values
-(1, 'Manual'),
-(2, 'OpenET'),
-(3, 'CIMIS'),
-(4, 'Trade')
+(1, 'Manual', 'Manual'),
+(2, 'OpenET', 'Open ET'),
+(3, 'CIMIS', 'CIMIS'),
+(4, 'Trade', 'Trade')
 
 go 
 
-alter table dbo.ParcelLedger
-add ParcelLedgerEntrySourceTypeID int not null constraint FK_ParcelLedger_ParcelLedgerEntrySourceType_ParcelLedgerEntrySourceTypeID default 1
+alter table dbo.ParcelLedger add ParcelLedgerEntrySourceTypeID int null constraint FK_ParcelLedger_ParcelLedgerEntrySourceType_ParcelLedgerEntrySourceTypeID
 foreign key (ParcelLedgerEntrySourceTypeID) references dbo.ParcelLedgerEntrySourceType(ParcelLedgerEntrySourceTypeID)
 
 go 
+
+update dbo.ParcelLedger set ParcelLedgerEntrySourceTypeID = 1
+
+alter table dbo.ParcelLedger alter column ParcelLedgerEntrySourceTypeID int not null
+GO
 
 -- Allocation, Manual Adjustment as Manual
 update dbo.ParcelLedger
