635b1d968ff6623a4eeb65644056a7d5f99ea721
Mack Peters <mack.peters@sitkatech.com>
2021-12-02 15:44:53 -0800

    A few more necessary updates to facilitate the high likelihood of  multiple adjustments

--- Database/Objects/Procs/dbo.pUpdateParcelMonthlyEvapotranspirationWithETData.sql
+++ Database/Objects/Procs/dbo.pUpdateParcelMonthlyEvapotranspirationWithETData.sql
@@ -13,10 +13,11 @@ begin
 	declare @effectiveDateMonth int
 	declare @effectiveDateYear int
 
-	select top 1 @effectiveDate = dateadd(day, -1, dateadd(month, 1, cast(concat(WaterMonth, '/1/', WaterYear) as datetime))), @effectiveDateMonth = WaterMonth, @effectiveDateYear = WaterYear
+	select top 1 @effectiveDate = dateadd(hour, 8,(dateadd(day, -1, dateadd(month, 1, cast(concat(WaterMonth, '/1/', WaterYear) as datetime))))), @effectiveDateMonth = WaterMonth, @effectiveDateYear = WaterYear
 	from dbo.OpenETGoogleBucketResponseEvapotranspirationData
 
-	drop table if exists #npla
+	drop table if exists #npl
+	drop table if exists #openETPLEntriesForDate
 
 	select p.ParcelID, p.ParcelAreaInAcres, @effectiveDate as EffectiveDate, -((et.EvapotranspirationRateInches / 12) * p.ParcelAreaInAcres) as TransactionAmount
 	into #npl
@@ -24,26 +25,31 @@ begin
 	join dbo.OpenETGoogleBucketResponseEvapotranspirationData et
 	on p.ParcelNumber = et.ParcelNumber
 
+	select ParcelID, EffectiveDate, sum(TransactionAmount) TransactionAmount, max(ParcelLedgerID) ParcelLedgerID
+	into #openETPLEntriesForDate
+	from dbo.ParcelLedger pal
+	where EffectiveDate = @effectiveDate and TransactionTypeID in (17,18)
+	group by ParcelID, EffectiveDate
+
 	-- insert any corrections first - changes in value
 	insert into dbo.ParcelLedger(ParcelID, TransactionTypeID, TransactionDate, EffectiveDate, TransactionAmount, TransactionDescription)
 	select npl.ParcelID, 18 as TransactionTypeID, @transactionDate as TransactionDate, npl.EffectiveDate, (abs(pl.TransactionAmount) - abs(npl.TransactionAmount)) as TransactionAmount, concat('A correction to ', @effectiveDateMonth, '/', @effectiveDateYear, ' has been applied to this water account') as TransactionDescription
 	from #npl npl
-	join dbo.ParcelLedger pl on npl.ParcelID = pl.ParcelID and npl.EffectiveDate = pl.EffectiveDate and pl.WaterTypeID is null
+	join #openETPLEntriesForDate pl on npl.ParcelID = pl.ParcelID and npl.EffectiveDate = pl.EffectiveDate
 	where npl.TransactionAmount != pl.TransactionAmount
 
 	-- insert any corrections first - if they are no longer in the ET dataset we should treat it as a correction
 	insert into dbo.ParcelLedger(ParcelID, TransactionTypeID, TransactionDate, EffectiveDate, TransactionAmount, TransactionDescription)
 	select pl.ParcelID, 18 as TransactionTypeID, @transactionDate as TransactionDate, pl.EffectiveDate, -pl.TransactionAmount, concat('A correction to ', @effectiveDateMonth, '/', @effectiveDateYear, ' has been applied to this water account') as TransactionDescription
-	from dbo.ParcelLedger pl 
+	from #openETPLEntriesForDate pl 
 	left join #npl npl on pl.ParcelID = npl.ParcelID and pl.EffectiveDate = npl.EffectiveDate
-	--OpenET Ledger entries have a null WaterTypeID
-	where npl.ParcelID is null and pl.WaterTypeID is null and pl.EffectiveDate = @effectiveDate
+	where npl.ParcelID is null
 
 	-- then add any new ones
 	insert into dbo.ParcelLedger(ParcelID, TransactionTypeID, TransactionDate, EffectiveDate, TransactionAmount, TransactionDescription)
 	select npl.ParcelID, 17 as TransactionTypeID, @transactionDate as TransactionDate, npl.EffectiveDate, npl.TransactionAmount, concat(@effectiveDateMonth, '/', @effectiveDateYear, ' Usage from OpenET has been withdrawn from this water account') as TransactionDescription
 	from #npl npl
-	left join dbo.ParcelLedger pl on npl.ParcelID = pl.ParcelID and npl.EffectiveDate = pl.EffectiveDate and pl.WaterTypeID is null
+	left join #openETPLEntriesForDate pl on npl.ParcelID = pl.ParcelID and npl.EffectiveDate = pl.EffectiveDate
 	where pl.ParcelLedgerID is null
 
 end
