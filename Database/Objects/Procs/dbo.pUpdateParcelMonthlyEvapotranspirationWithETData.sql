e1a1d9552d64df9628bcf8838cd8e6a179ebdeb1
jamie.quishenberry <jamie.quishenberry@sitkatech.com>
2021-12-06 17:43:10 -0800

    Updated to reference new ParcelLedger data model

--- Database/Objects/Procs/dbo.pUpdateParcelMonthlyEvapotranspirationWithETData.sql
+++ Database/Objects/Procs/dbo.pUpdateParcelMonthlyEvapotranspirationWithETData.sql
@@ -15,23 +15,25 @@ begin
 	join dbo.OpenETGoogleBucketResponseEvapotranspirationData et
 	on p.ParcelNumber = et.ParcelNumber
 
+	-- TransactionTypeID = 18 => TransactionTypeID = 2, ParcelLedgerEntrySourceTypeID = 2
+
 	-- insert any corrections first - changes in value
 	insert into dbo.ParcelLedger(ParcelID, TransactionTypeID, TransactionDate, EffectiveDate, TransactionAmount)
-	select npl.ParcelID, 18 as TransactionTypeID, @transactionDate as TransactionDate, npl.EffectiveDate, (abs(pl.TransactionAmount) - abs(npl.TransactionAmount)) as TransactionAmount
+	select npl.ParcelID, 2 as TransactionTypeID, 2 as ParcelLedgerEntrySourceTypeID, @transactionDate as TransactionDate, npl.EffectiveDate, (abs(pl.TransactionAmount) - abs(npl.TransactionAmount)) as TransactionAmount
 	from #npl npl
 	join dbo.ParcelLedger pl on npl.ParcelID = pl.ParcelID and npl.EffectiveDate = pl.EffectiveDate
 	where pl.ParcelLedgerID is null	and npl.TransactionAmount != pl.TransactionAmount
 
 	-- insert any corrections first - if they are no longer in the ET dataset we should treat it as a correction
 	insert into dbo.ParcelLedger(ParcelID, TransactionTypeID, TransactionDate, EffectiveDate, TransactionAmount)
-	select pl.ParcelID, 18 as TransactionTypeID, @transactionDate as TransactionDate, pl.EffectiveDate, -pl.TransactionAmount
+	select pl.ParcelID, 2 as TransactionTypeID, 2 as ParcelLedgerEntrySourceTypeID, @transactionDate as TransactionDate, pl.EffectiveDate, -pl.TransactionAmount
 	from dbo.ParcelLedger pl 
 	left join #npl npl on pl.ParcelID = npl.ParcelID and pl.EffectiveDate = npl.EffectiveDate
 	where npl.ParcelID is null
 
 	-- then add any new ones
 	insert into dbo.ParcelLedger(ParcelID, TransactionTypeID, TransactionDate, EffectiveDate, TransactionAmount)
-	select npl.ParcelID, 17 as TransactionTypeID, @transactionDate as TransactionDate, npl.EffectiveDate, npl.TransactionAmount
+	select npl.ParcelID, 2 as TransactionTypeID, 2 as ParcelLedgerEntrySourceTypeID, @transactionDate as TransactionDate, npl.EffectiveDate, npl.TransactionAmount
 	from #npl npl
 	left join dbo.ParcelLedger pl on npl.ParcelID = pl.ParcelID and npl.EffectiveDate = pl.EffectiveDate
 	where pl.ParcelLedgerID is null
