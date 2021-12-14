7cf4f1ed2512b8911b00fc0c390cf508f2a48269
jamie.quishenberry <jamie.quishenberry@sitkatech.com>
2021-12-13 18:40:23 -0800

    Create transaction from spreadsheet upload form laid out, link to form added from createWaterTransaction page. CustomRichTextType enum updated to include createTransactionFromSpreadsheet type.

--- Source/Rio.Web/src/app/pages/parcel-ledger-create-from-spreadsheet/parcel-ledger-create-from-spreadsheet.component.ts
+++ Source/Rio.Web/src/app/pages/parcel-ledger-create-from-spreadsheet/parcel-ledger-create-from-spreadsheet.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { WaterTypeService } from 'src/app/services/water-type.service';
+import { UserDto } from 'src/app/shared/generated/model/user-dto';
+import { WaterTypeDto } from 'src/app/shared/generated/model/water-type-dto';
+import { CustomRichTextType } from 'src/app/shared/models/enums/custom-rich-text-type.enum';
 
 @Component({
   selector: 'rio-parcel-ledger-create-from-spreadsheet',
@@ -7,9 +12,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParcelLedgerCreateFromSpreadsheetComponent implements OnInit {
 
-  constructor() { }
+  private watchUserChangeSubscription: any;
+  private currentUser: UserDto;
+  public richTextTypeID = CustomRichTextType.ParcelLedgerCreateFromSpreadsheet;
+  public waterTypes: WaterTypeDto[];
+  public isLoadingSubmit: boolean = false;
+  public fileUpload: File;
+
+  constructor(
+    private waterTypeService: WaterTypeService,
+    private authenticationService: AuthenticationService,
+    private cdr: ChangeDetectorRef,
+  ) { }
 
   ngOnInit(): void {
+    this.watchUserChangeSubscription = this.authenticationService.currentUserSetObservable.subscribe((currentUser) => {
+      this.currentUser = currentUser;
+
+      this.waterTypeService.getWaterTypes().subscribe(waterTypes => {
+        // only grab user defined water types
+        this.waterTypes = waterTypes.filter(x => x.IsUserDefined);
+      });
+    });
   }
 
+  ngOnDestroy() {
+    this.watchUserChangeSubscription.unsubscribe();
+    this.authenticationService.dispose();
+    this.cdr.detach();
+  }
+
+  public onSubmit(createTransactionFromSpreadsheetForm: HTMLFormElement) { }
 }
