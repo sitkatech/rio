7f218cdb7dbc79e099f6e0b9590ffb54c89eba24
Mack Peters <mack.peters@sitkatech.com>
2022-02-09 08:13:58 -0800

    RIO-365 additional work. Needed to alter the database object directly and not alter them in a release script. I've been out of the SPA game too long!

--- Database/Objects/Procs/dbo.pParcelWaterSupplyAndUsage.sql
+++ Database/Objects/Procs/dbo.pParcelWaterSupplyAndUsage.sql
@@ -11,8 +11,8 @@ as
 begin
 
 	select p.ParcelID, p.ParcelNumber, p.ParcelAreaInAcres,
-			pal.ManualSupply + pal.Precipitation + pal.Purchased - pal.Sold as TotalSupply, 
-			pal.Precipitation, pal.Purchased, pal.Sold, pal.UsageToDate, a.AccountID, a.AccountName, a.AccountNumber
+			pal.ManualSupply + pal.Purchased - pal.Sold as TotalSupply, 
+			pal.Purchased, pal.Sold, pal.UsageToDate, a.AccountID, a.AccountName, a.AccountNumber
 	from dbo.Parcel p
 	join dbo.AccountParcelWaterYear apwy on p.ParcelID = apwy.ParcelID
 	join dbo.WaterYear wy on apwy.WaterYearID = wy.WaterYearID and wy.[Year] = @year
@@ -21,7 +21,6 @@ begin
 	(
 		select pl.ParcelID, 
 			isnull(sum(case when pl.TransactionTypeID = 1 and pl.ParcelLedgerEntrySourceTypeID = 1 then pl.TransactionAmount else 0 end), 0) as ManualSupply,
-			isnull(sum(case when pl.ParcelLedgerEntrySourceTypeID = 3 then pl.TransactionAmount else 0 end), 0) as Precipitation,
 			isnull(sum(case when pl.ParcelLedgerEntrySourceTypeID = 4 and pl.TransactionAmount > 0 then pl.TransactionAmount else 0 end), 0) as Purchased,
 			abs(isnull(sum(case when pl.ParcelLedgerEntrySourceTypeID = 4 and pl.TransactionAmount < 0 then pl.TransactionAmount else 0 end), 0)) as Sold,
 			abs(sum(case when pl.TransactionTypeID = 2 then pl.TransactionAmount else 0 end)) as UsageToDate
