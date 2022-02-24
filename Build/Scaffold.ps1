db459f63c352798231d0cb18fec7e41ce94ab15a
raynardlee <raynardlee@yahoo.com>
2022-02-23 16:02:42 -0800

    moving values out to build.ini

--- Build/Scaffold.ps1
+++ Build/Scaffold.ps1
@@ -1,20 +1,23 @@
-$projectPath = "C:\git\sitkatech\edf"
-$apiModelsPath = $projectPath + "\Rio.Models\DataTransferObjects\Generated"
-$apiEFModelsPath = $projectPath + "\Rio.EFModels\Entities\Generated"
-$apiEFModelExtensionMethodsPath = $apiEFModelsPath + "\ExtensionMethods"
-$apiEFModelsDbContextPath = $apiEFModelsPath + "\RioDbContext.cs"
+Param(
+  [Parameter (Mandatory = $false)]
+  [string] $iniFile = ".\build.ini"
+)
+
+Import-Module .\Get-Config.psm1
+
+$config = Get-Config -iniFile $iniFile 
 
 "Scaffold"
-& Scaffold-DbContext "Server=.\;Database=EDFDB;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Entities/Generated -Project Rio.EFModels -Context "RioDbContext" -Force -StartupProject Rio.EFModels -DataAnnotations -UseDatabaseNames
+& Scaffold-DbContext "Server=$config.Server;Database=$config.DatabaseName;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Entities/Generated -Project Rio.EFModels -Context "RioDbContext" -Force -StartupProject Rio.EFModels -DataAnnotations -UseDatabaseNames
 
 # fix the DbContext OnConfiguring line
 
-(Get-Content -Raw $apiEFModelsDbContextPath) -replace 'protected override void OnConfiguring\(DbContextOptionsBuilder optionsBuilder\)\s*{\s*if \(!optionsBuilder.IsConfigured\)\s*{[^}]*}\s*}', '' | Out-File $apiEFModelsDbContextPath -encoding ascii
+(Get-Content -Raw $config.ApiEFModelsDbContextPath) -replace 'protected override void OnConfiguring\(DbContextOptionsBuilder optionsBuilder\)\s*{\s*if \(!optionsBuilder.IsConfigured\)\s*{[^}]*}\s*}', '' | Out-File $config.ApiEFModelsDbContextPath -encoding ascii
 
 # remove .Generated from the namespace
 
-ForEach ($File in (Get-ChildItem -Path $apiEFModelsPath -Recurse -File)) {
+ForEach ($File in (Get-ChildItem -Path $config.ApiEFModelsPath -Recurse -File)) {
     (Get-Content -Raw $File.FullName) -replace 'Entities.Generated', 'Entities' | Out-File $File.FullName -encoding ascii
 }
 
-Start-Process -Wait -FilePath "C:\git\sitkatech\efcorepocogenerator\EFCorePOCOGenerator\bin\Debug\net5.0\EFCorePOCOGenerator.exe" --db-name="EDFDB", --generate-simple-dtos=true, --csharp-dto-namespace="Rio.Models.DataTransferObjects", --code-namespace="Rio.EFModels.Entities", --api-efmodels-output-dir=$apiEFModelExtensionMethodsPath, --api-models-output-dir=$apiModelsPath, --table-exclude-list="DatabaseMigration"
+Start-Process -Wait -FilePath "C:\git\sitkatech\efcorepocogenerator\EFCorePOCOGenerator\bin\Debug\net5.0\EFCorePOCOGenerator.exe" --db-name=$config.DatabaseName, --generate-simple-dtos=true, --csharp-dto-namespace="Rio.Models.DataTransferObjects", --code-namespace="Rio.EFModels.Entities", --api-efmodels-output-dir=$config.ApiEFModelExtensionMethodsPath, --api-models-output-dir=$config.ApiModelsPath, --table-exclude-list="DatabaseMigration"
