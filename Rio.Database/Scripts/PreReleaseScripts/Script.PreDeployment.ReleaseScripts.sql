ea5a284ee50027d9d26f3e9c13570f28fdc3d6b7
raynardlee <raynardlee@yahoo.com>
2022-02-27 11:05:25 -0800

    added prerelease script to remove Precip custom rich text

--- Rio.Database/Scripts/PreReleaseScripts/Script.PreDeployment.ReleaseScripts.sql
+++ Rio.Database/Scripts/PreReleaseScripts/Script.PreDeployment.ReleaseScripts.sql
@@ -6,11 +6,8 @@ This file is generated on every build, DO NOT modify.
 */
 
 PRINT N'Rio.Database - Script.PreDeployment.ReleaseScripts.sql';
-
-delete
-from dbo.CustomRichText
-where CustomRichTextTypeID = 27
-
 GO
 
+:r ".\Remove Unused CustomRichText.sql"
+GO
 
