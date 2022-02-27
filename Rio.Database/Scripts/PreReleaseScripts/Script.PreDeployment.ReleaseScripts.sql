ee89ad406fcb36c60a36a290bf25af1e06ed0628
raynardlee <raynardlee@yahoo.com>
2022-02-27 10:32:59 -0800

    need to delete CustomRichTextTypeID 27; trying it out as a PreReleaseScript

--- Rio.Database/Scripts/PreReleaseScripts/Script.PreDeployment.ReleaseScripts.sql
+++ Rio.Database/Scripts/PreReleaseScripts/Script.PreDeployment.ReleaseScripts.sql
@@ -6,6 +6,11 @@ This file is generated on every build, DO NOT modify.
 */
 
 PRINT N'Rio.Database - Script.PreDeployment.ReleaseScripts.sql';
+
+delete
+from dbo.CustomRichText
+where CustomRichTextTypeID = 27
+
 GO
 
 
