8188d8aca1cd0523faad8d238ea2ff6781b849d4
raynardlee <raynardlee@yahoo.com>
2022-04-01 13:26:42 -0700

    hate this stupid sqlproj being undeterministic; making transactiontypedisplayname nullable for now and then will change the schema later after deployment

--- Rio.Database/Scripts/PreReleaseScripts/Script.PreDeployment.ReleaseScripts.sql
+++ Rio.Database/Scripts/PreReleaseScripts/Script.PreDeployment.ReleaseScripts.sql
@@ -8,8 +8,4 @@ This file is generated on every build, DO NOT modify.
 PRINT N'Rio.Database - Script.PreDeployment.ReleaseScripts.sql';
 GO
 
-:r ".\001- Making TransactionType a lookup table.sql"
-GO
-:r ".\002- Inserting values to TransactionType.sql"
-GO
 
