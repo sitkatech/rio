e04f7ab945143b91cd3831fdf04179caafda9aa3
raynardlee <raynardlee@yahoo.com>
2022-04-02 11:03:46 -0700

    adding water years to initial data script

--- Rio.Database/Scripts/ReleaseScripts/0001 - Initial data script.sql
+++ Rio.Database/Scripts/ReleaseScripts/0001 - Initial data script.sql
@@ -58,6 +58,14 @@ INSERT [dbo].[CustomRichText] ([CustomRichTextID], [CustomRichTextTypeID], [Cust
 
     set identity_insert dbo.CustomRichText off
 
+insert into dbo.WaterYear ([Year])
+values (2021), (2022)
+
+insert into dbo.WaterYearMonth (WaterYearID, Month)
+values 
+(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6), (1, 7), (1, 8), (1, 9), (1, 10), (1, 11), (1, 12),
+(2, 1), (2, 2), (2, 3), (2, 4), (2, 5), (2, 6), (2, 7), (2, 8), (2, 9), (2, 10), (2, 11), (2, 12)
+
     INSERT INTO dbo.DatabaseMigration(MigrationAuthorName, ReleaseScriptFileName, MigrationReason)
     SELECT 'Ray Lee', @MigrationName, 'Add initial data to the system.'
 END
